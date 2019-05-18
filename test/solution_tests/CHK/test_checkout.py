eceb4700e57ef95eaf772a14464fce9a7011058b
sbx_user1051 <sbx_user1051@169.254.14.253>
2019-05-18 11:52:48 +0000

    Sat May 18 11:52:48 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -16,30 +16,38 @@ class TestItem:
         assert item.checkout(2) == 15
         assert item.checkout(3) == 25
 
+    def test_item_checkout_multi_discount(self):
+        item = Item(10, ((2, 15), (3, 12)))
+
+        assert item.checkout(1) == 10
+        assert item.checkout(2) == 15
+        assert item.checkout(3) == 12
+        assert item.checkout(4) == 22
+        assert item.checkout(5) == 27
+        assert item.checkout(6) == 24
 
 
 class TestCheckout:
     def test_checkout_error(self):
         invalid_inputs = (
-            None, ["A"], 1, {"A": "A"}, "E", "ABCDE",
+            None, ["A"], 1, {"A": "A"}, "X", "ABCDX",
         )
 
         for input_ in invalid_inputs:
             assert checkout(input_) == -1
 
 def test_checkout_ok(self):
-        # well i am assuming that the input will be something like:
-        # "ABAABC"
-
         in_out = {
             "": 0,  # not sure about this one
             "A": 50, "AB": 80, "C": 20,
             "CCCCC": 100, "AAA": 130, "AAAA": 180,
-            "DABDBC": 145
+            "DABDBC": 145,
+
         }
 
         for input_, output in in_out.items():
             assert checkout(input_) == output, input_
+
 
 
 
