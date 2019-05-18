7fd9fb272bb28d35fa426691c9c55a9bc1971791
sbx_user1051 <sbx_user1051@169.254.14.253>
2019-05-18 10:44:40 +0000

    Sat May 18 10:44:40 UTC 2019

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,4 +3,5 @@ from lib.solutions.HLO.hello_solution import hello
 
 class TestHello:
     def test_hello(self):
-        assert hello("") == "Hello World"
+        assert hello("") == "Hello, World!"
+
