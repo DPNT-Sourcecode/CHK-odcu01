408a4cb77a2b5174ba83189e0e512d6988bfeae7
sbx_user1051 <sbx_user1051@169.254.14.253>
2019-05-18 11:07:05 +0000

    Sat May 18 11:07:05 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,21 +1,22 @@
-
-
 class Item:
-    def __int__(self):
-        pass
+    def __init__(self, price: int, special_offer: tuple) -> None:
+        self.price = price
+        self.special_offer = special_offer
 
     def checkout(self, n: int) -> int:
         pass
 
 
 PRICE_TABLE = {
-    "A": (50, (3, 130)),
-    "B": ()
+    "A": Item(50, (3, 130)),
+    "B": Item(30, (2, 130)),
+    "C": Item(20, (3, 130)),
+    "D": Item(50, (3, 130)),
 }
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
 
