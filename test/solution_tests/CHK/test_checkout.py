aaaaeb8115776950a3abe26a5cd3c4ef2cfacd5f
sbx_user1051 <sbx_user1051@169.254.14.253>
2019-05-18 10:58:05 +0000

    Sat May 18 10:58:05 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,11 +4,23 @@ from lib.solutions.CHK.checkout_solution import checkout
 class TestCheckout:
     def test_checkout_error(self):
         invalid_inputs = (
-            None, ["A"], 1, {"A": "A"}, "D"
+            None, ["A"], 1, {"A": "A"}, "E"
         )
 
         for input_ in invalid_inputs:
             assert checkout(input_) == -1
 
-    
+    def test_checkout_ok(self):
+        # well i am assuming that the input will be something like:
+        # "ABAABC"
+
+        in_out = {
+            "": 0, # not sure about this one
+            "A": 50,
+            "AB": 80,
+            "C": 20,
+            "CCCCC": 100,
+            
+        }
+
 
