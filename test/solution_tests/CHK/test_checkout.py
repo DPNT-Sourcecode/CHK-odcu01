97823666e15eb985578244eb936fcf05ff842526
sbx_user1051 <sbx_user1051@169.254.14.253>
2019-05-18 10:55:05 +0000

    Sat May 18 10:55:05 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,3 +3,12 @@ from lib.solutions.CHK.checkout_solution import checkout
 
 class TestCheckout:
     def test_checkout_error(self):
+        invalid_inputs = (
+            None, ["A"], 1, {"A": "A"}, "D"
+        )
+
+        for input_ in invalid_inputs:
+            assert checkout(input_) == -1
+
+    
+
