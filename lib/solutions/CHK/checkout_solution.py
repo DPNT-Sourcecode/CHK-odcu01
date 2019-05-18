a2b7503e0e49c491a27487589d05867bd0474670
sbx_user1051 <sbx_user1051@169.254.14.253>
2019-05-18 11:04:05 +0000

    Sat May 18 11:04:05 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,21 @@
 
 
+class Item:
+    def __int__(self):
+        pass
+
+    def checkout(self, n: int) -> int:
+        pass
+
+
+PRICE_TABLE = {
+    "A": (50, (3, 130)),
+    "B": ()
+}
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
